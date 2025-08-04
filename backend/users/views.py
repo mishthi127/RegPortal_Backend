1319200918ba6ee213574bb2114768e7b9051873
Silpibora <silpipriyabora@gmail.com>
2025-08-05 00:13:41 +0530

    okk

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 # Create your views here.
+#serializers means convert the queryset data into JSON format 
+
+@api_view(['GET'])
